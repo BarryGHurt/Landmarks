397cc00d0b436e3e72d2bd243f66f0a19870ab88
Barry Hurt <barry.hurt@protonmail.com>
2024-01-30 14:55:46 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Barry Hurt on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
