6e7964f923a8f9d34a92a5d3d6b61a950353c544
Barry Hurt <barry.hurt@protonmail.com>
2024-02-20 15:23:38 -0800

    UIkit watch app

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Barry Hurt on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
